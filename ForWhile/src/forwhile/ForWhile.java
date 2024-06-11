929fc1973a6369a8d16e92472687d9ba34b3ddf7
YamilaVallejo <you@example.com>
2024-06-11 17:07:32 -0600

    for-While

---
+++ ForWhile/src/forwhile/ForWhile.java
@@ -0,0 +1,24 @@
+
+package forwhile;
+
+/**
+ *
+ * @author Yamila
+ */
+public class ForWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for (int i= 1; i<=10; ++i){
+           System.out.println("FOR Numeros "+i);
+       }
+       int j=1;
+       while (j<=10){
+           System.out.println("WHILE Numeros: " + j);
+           j++;
+       }
+    }
+    
+}
